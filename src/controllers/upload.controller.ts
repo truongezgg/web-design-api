065c41561fcddae33fb9f599c9fa21d3eb1f3144
Truongezgg <truongezgg@gmail.com>
2022-06-21 21:43:23 +0700

    test API upload

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -3,24 +3,29 @@ import { fail, success } from '$helpers/response';
 import { Express, Request, Response } from 'express';
 import { verifyAccessToken } from '$middlewares/auth.middleware';
 import config from '$config';
+import s3Upload from '$helpers/s3Upload';
 
 const logger = log('Upload controller');
 
 export default function uploadController(app: Express) {
-  app.post('/upload', [verifyAccessToken], async (req: Request, res: Response) => {
-    try {
-      let files = [];
+  app.post(
+    '/upload',
+    [verifyAccessToken, s3Upload.upload.array('files', 10)],
+    async (req: Request, res: Response) => {
+      try {
+        let files = [];
 
-      if (req['files'] && req['files'].length > 0) {
-        for (let f of req['files']) {
-          files.push(`${config.S3.S3_DOMAIN}/${f.key}`);
+        if (req['files'] && req['files'].length > 0) {
+          for (let f of req['files']) {
+            files.push(`${config.S3.S3_DOMAIN}/${f.key}`);
+          }
         }
-      }
 
-      return success(res, files);
-    } catch (err) {
-      logger.error(err);
-      return fail(res, err);
+        return success(res, files);
+      } catch (err) {
+        logger.error(err);
+        return fail(res, err);
+      }
     }
-  });
+  );
 }
